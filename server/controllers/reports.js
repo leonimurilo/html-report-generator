732c0961a7ef8ff509f23f84a76c1b929f47cdea
leoni <leonimega@hotmail.com>
2019-03-30 17:10:15 -0300

    add more stuff

--- server/controllers/reports.js
+++ server/controllers/reports.js
@@ -1,11 +1,54 @@
-const fs = require('fs');
-const path = require('path');
+import * as pug from 'pug';
+import puppeteer from 'puppeteer';
+import * as sass from 'node-sass';
+import * as path from 'path';
+
+const defaultOptions = {
+  htmlTemplatePath: path.resolve(__dirname, '../templates/pdf-template.pug'),
+  styleOptions: {
+    file: path.resolve(__dirname, '../templates/pdf-template.scss')
+  },
+  htmlTemplateOptions: {},
+  pdfOptions: {
+    path: 'pdf-file.pdf',
+    format: 'A4',
+    printBackground: true
+  }
+};
 
 module.exports = {
-  queue: (msg) => {
+  queue: async (options = defaultOptions) => {
+    const browser = await puppeteer.launch(options.puppeteerOptions);
+    const page = await browser.newPage();
+    let htmlTemplateOptions = { ...options.htmlTemplateOptions };
+    let renderedTemplate;
 
-    return new Promise((resolve, reject) => {
+    // This is conditional since the user could get his style in some other way.
+    if (options.styleOptions) {
+      const compiledStyle = sass.renderSync({ ...options.styleOptions });
 
-    });
+      htmlTemplateOptions = {
+        ...options.htmlTemplateOptions,
+        compiledStyle: compiledStyle.css,
+      };
+    }
+
+    if (options.htmlTemplateFn) {
+      renderedTemplate = options.htmlTemplateFn(htmlTemplateOptions);
+    } else if (options.htmlTemplatePath) {
+      renderedTemplate = pug.renderFile(options.htmlTemplatePath, htmlTemplateOptions);
+    } else {
+      throw Error('htmlTemplateFn or htmlTemplatePath must be provided');
+    }
+
+    // Make puppeteer render the HTML from data buffer
+    await page.goto(`data:text/html,${renderedTemplate}`,
+      { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
+
+    const pdfBuffer = await page.pdf({ ...options.pdfOptions });
+
+    await browser.close();
+
+    return pdfBuffer;
   }
 };
